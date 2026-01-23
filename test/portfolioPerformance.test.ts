e32188cb313c0f54d8838bcc23e1c8d626de7232
August <mturzak@rrc.ca>
2026-01-23 00:20:20 -0600

    Added endpoint test for performance, passes

--- test/portfolioPerformance.test.ts
+++ test/portfolioPerformance.test.ts
@@ -1,5 +1,18 @@
-
 import { calculatePortfolioPerformance, Portfolio } from "../src/portfolio/portfolioPerformance";
+import request, { Response } from "supertest";
+import app from "../src/app";
+
+describe("GET /api/v1/portfolio/performance", () => {
+    it("should return portfolio response", async () => {
+        const response: Response = await request(app).get("/api/v1/portfolio/performance").query({ initialInvestment: 1000, currentValue: 1000 });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("initialInvestment", 1000);
+        expect(response.body).toHaveProperty("currentValue", 1000);
+        expect(response.body).toHaveProperty("profitOrLoss"), 0;
+        expect(response.body).toHaveProperty("percentageChange", 0);
+        expect(response.body).toHaveProperty("performanceSummary", "No Change. Your portfolio is holding steady.");
+    });
+});
 
 describe("calculatePortfolioPerformance", () => {
     it("should return no change", () => {
