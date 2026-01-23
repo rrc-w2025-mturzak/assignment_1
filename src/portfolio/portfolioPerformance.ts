32f1c5a2748353ca868c6309a248a9deffece885
August <mturzak@rrc.ca>
2026-01-22 22:29:29 -0600

    Fixed calculatePortfolioPerformance function to calculate properly and added new investment cases

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -11,14 +11,33 @@ export function calculatePortfolioPerformance(portfolio: Portfolio): any {
     let currentValue = portfolio.currentValue;
 
     const profitOrLoss = currentValue - initialInvestment;
-
     const percentageChange = (profitOrLoss / initialInvestment) * 100;
 
     let performanceSummary;
-    if (percentageChange > 20) {
-        performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
-    } else {
-        performanceSummary = `The portfolio has performed poorly.`;
+    switch (true) {
+        case percentageChange >= 30:
+            performanceSummary = "Excellent Performance! Your investments are doing great."
+            break
+
+        case percentageChange >= 10:
+            performanceSummary = "Solid gain. Keep monitoring your investments."
+            break
+
+        case percentageChange > 0:
+            performanceSummary = "Modest gain. Your portfolio is growing slowly"
+            break
+
+        case percentageChange == 0:
+            performanceSummary = "No Change. Your portfolio is holding steady."
+            break
+
+        case percentageChange < 0 && percentageChange >= -10:
+            performanceSummary = "Minor loss. Stay calm and review your options."
+            break
+
+        case percentageChange < -10:
+            performanceSummary = "Signifigant loss. Review your portfolio strategy."
+            break
     }
 
     return {
